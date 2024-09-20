89fe40a155217bfd9527ee14d8df3826810210cf
LouieGGG <lou.albalat.93@gmail.com>
2024-09-20 21:00:59 +0200

    Prettier

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 "use strict";
 import dotenv from "dotenv";
-dotenv.config();   
+dotenv.config();
 
 import express from "express";
 
 const app = express();
 
-const {PORT} = process.env;
+const { PORT } = process.env;
 
+//login
+app.get("/login", function (req, res) {
+  res.send("Login");
+});
 
 //Comentario
 app.get("/", function (req, res) {
-    res.send("Hello world");
+  res.send("Hello world");
 });
 
 app.listen(PORT, () => {
-    console.log(`http://127.0.0.1:${PORT}`);
+  console.log(`http://127.0.0.1:${PORT}`);
 });
-
